4701a8103c6e97ae6a7d0c9945450d7b8b8303b6
Javier Cardona <javier@okio.io>
2017-03-20 18:46:35 -0700

    Document how to install vim plugins

--- install.sh
+++ install.sh
@@ -13,3 +13,8 @@ echo =========================================
 echo Now add the following line to your .bashrc
 echo
 echo   'for DOTFILE in ~/dot-files/system/*; do [ -f ${DOTFILE} ] && source ${DOTFILE}; done'
+echo
+echo And next time you fire vim, run
+echo
+echo :PluginInstall
+echo
